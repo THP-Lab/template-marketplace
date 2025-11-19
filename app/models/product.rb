b79334e3d327fd336e71a64c331d8cc15bfea9f0
jobeejobaa <johannadelfieux@gmail.com>
2025-11-19 16:01:20 +0100

    product scafold

---
+++ app/models/product.rb
@@ -0,0 +1,8 @@
+class Product < ApplicationRecord
+  has_many :cart_products
+  has_many :order_products
+  has_many :carts, through: :cart_products
+  has_many :orders, through: :order_products
+
+  
+end
