0ce22e18c2dbe53f6d2a2dfa8e3158f8ff0b5e05
jobeejobaa <johannadelfieux@gmail.com>
2025-11-19 16:13:13 +0100

    restriction user

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
-  before_action :set_product, only: %i[ show edit update destroy ]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :check_admin, only: [:new, :create, :edit, :update, :destroy]
 
   # GET /products or /products.json
   def index
