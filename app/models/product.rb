1dcfcfe40c147b7a08cebc2b99fa226e9cc818bc
jobeejobaa <johannadelfieux@gmail.com>
2025-11-20 15:50:55 +0100

    remove validation in model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,2 @@
-class Product < ApplicationRecord
-  has_many :cart_products
-  has_many :order_products
-  has_many :carts, through: :cart_products
-  has_many :orders, through: :order_products
-
-  
+class Product < ApplicationRecord 
 end
