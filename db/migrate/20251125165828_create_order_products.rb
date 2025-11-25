92efb895683f0276ded563e4ade834dde09aa981
Miranda <lucasmiranda.pro@gmail.com>
2025-11-25 18:20:38 +0100

    switch card product to order product

--- db/migrate/20251125165828_create_order_products.rb
+++ db/migrate/20251125165828_create_order_products.rb
@@ -3,7 +3,7 @@ class CreateOrderProducts < ActiveRecord::Migration[8.1]
     create_table :order_products do |t|
       t.references :order, null: false, foreign_key: true
       t.references :product, null: false, foreign_key: true
-      t.integer :quantity
+      t.integer :quantity, default: 1
       t.decimal :unit_price
 
       t.timestamps
