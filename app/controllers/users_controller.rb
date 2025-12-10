928865da1ab91cf7dbcdefb5e7bcdc146a4feb05
Istarengwa <lilian@thehackingproject.org>
2025-12-10 15:01:06 +0100

    add pagination system for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_admin!, only: [:admin]
   before_action :set_user
 
   def show
@@ -7,8 +8,8 @@ class UsersController < ApplicationController
   end
 
   def admin
-    require_admin!
     @users = User.order(created_at: :desc)
+    @users, @pagination = paginate(@users)
   end
 
   def update
